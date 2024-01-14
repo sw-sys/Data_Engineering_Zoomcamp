366430174a56a8d4fee656646767f5df6294208b
sw-sys <sheldonkorpet@gmail.com>
2024-01-14 13:39:55 +0000

    add pipeline files

---
+++ WK1_work/2_docker_sql/pipeline.py
@@ -0,0 +1,13 @@
+import sys
+import pandas as pd
+
+print(sys.argv)
+
+#sys.argv[0] is file
+day = sys.argv[1]
+
+# fancy stuff with pandas
+# load csv file
+
+# print status
+print(f'status: job finished successfully for day = {day}')
